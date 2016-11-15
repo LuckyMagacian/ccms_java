c378d593a7e33dbae4c8a225f84c8e49c64f5a23
yangyuanjian <yangyuanjian@188lanxi.com>
2016-11-15 16:41:38 +0800

    修复空指针

--- src/main/java/com/lanxi/service/impl/ActivityServiceImpl.java
+++ src/main/java/com/lanxi/service/impl/ActivityServiceImpl.java
@@ -172,8 +172,8 @@ public class ActivityServiceImpl implements ActivityService{
     		req.setCharacterEncoding("utf-8");
     		String actv_name    =req.getParameter("actv_name");
 	        String actv_state   =req.getParameter("actv_state");
-	        actv_name=actv_name.trim().isEmpty()?null:actv_name;
-	        actv_state=actv_state.trim().isEmpty()?null:actv_state;
+	        actv_name=actv_name!=null?actv_name.trim().isEmpty()?null:actv_name:null;
+	        actv_state=actv_state!=null?actv_state.trim().isEmpty()?null:actv_state:null;
 	        String date1		=req.getParameter("start1");
 	        String date2		=req.getParameter("start2");
 	        Date   start1		=null;
